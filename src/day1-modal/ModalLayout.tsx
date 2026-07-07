926337667393e01a2d4d48055fa62bf0ec73d73a
김상윤 <ksy151020@gmail.com>
2026-07-07 21:45:18 +0900

    feat: 모달 바깥영역을 클릭하면 닫히도록 기능 추가

--- src/day1-modal/ModalLayout.tsx
+++ src/day1-modal/ModalLayout.tsx
@@ -10,7 +10,11 @@ interface Props {
 
 export default function ModalLayout({ header, children, footer, handleClose }: Props) {
   return (
-    <Overlay>
+    <Overlay
+      onClick={(e) => {
+        if (e.target === e.currentTarget) handleClose();
+      }}
+    >
       <MainContainer>
         <HeaderContainer>
           <Header>{header}</Header>
@@ -19,23 +23,50 @@ export default function ModalLayout({ header, children, footer, handleClose }: P
         <ContentsContainer>
           <Contents>{children}</Contents>
         </ContentsContainer>
-        <FooterContainer>
-          <Footer>{footer}</Footer>
-        </FooterContainer>
+        <Footer>{footer}</Footer>
       </MainContainer>
     </Overlay>
   );
 }
 
-const Overlay = styled.div``;
-const MainContainer = styled.div``;
-const HeaderContainer = styled.div``;
-const ContentsContainer = styled.div``;
-const FooterContainer = styled.div``;
-const Header = styled.p``;
+const Overlay = styled.div`
+  position: fixed;
+  inset: 0;
+  background: #00000059;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+`;
+const MainContainer = styled.div`
+  width: 382px;
+  height: 614px;
+  border-radius: 8px;
+  padding: 24px 32px;
+  gap: 32px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+`;
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const ContentsContainer = styled.div`
+  flex: 1;
+`;
+
+const Header = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+  margin: 0;
+`;
 const Contents = styled.div``;
-const Footer = styled.div``;
+const Footer = styled.footer``;
 const CloseButton = styled.button`
+  color: #000000;
   background: none;
   border: none;
   cursor: pointer;
